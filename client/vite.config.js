0d72c54b771b98ee1608215d5190ba2ef8d6fb26
kodeeswaran-m <kodeeswaranmanjunathan@gmail.com>
2025-02-04 12:31:41 +0530

    dist changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',  // Ensure this matches your expected output folder
+    rollupOptions: {
+      input: 'index.html',  // Or your entry HTML file
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
